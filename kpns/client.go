93a4a457b0920d5ed55c4a779112c4fe434f4dbe
DasonYang <dasonyang@gmail.com>
2017-07-21 18:31:01 +0800

    mmh3 porting

--- kpns/client.go
+++ kpns/client.go
@@ -1,10 +1,31 @@
 package kpns
 
 import (
+    // "fmt"
     "log"
     "bytes"
-    "github.com/reusee/mmh3"
+    
+    // "encoding/binary"
+
+    "kpns/utils"
+
+    // "github.com/spaolacci/murmur3"
+    // "github.com/dmcgowan/mmh3"
+
+    // "github.com/reusee/mmh3"
+    // "strconv"
 )
+
+func divreverse(n uint64) uint64 {
+
+    var ret uint64
+    for i := n; i > 0; {
+        ret = ret<<1 + i%2
+        i = i / 2
+    }
+
+    return ret
+}
 
 type ClientData struct {
     Key     string                  `bson:"key"`
@@ -12,14 +33,38 @@ type ClientData struct {
 }
 
 func GetClientKey(udid string, appid string, platform string) string {
-    var key string
     var buffer bytes.Buffer
     buffer.WriteString(udid)
     buffer.WriteString(appid)
     buffer.WriteString(platform)
-    key = string(mmh3.Sum128(buffer.Bytes()))
+    var key string
 
-    log.Printf("hashed key = %v\n", key)
+    log.Printf("buffer = %v\n", buffer.String())
+
+    // key = utils.Mmh3py(buffer.String())
+    key = utils.Mmh3py("abc")
+
+    // rb := make([]byte, 16)
+
+    // for idx, item := range b {
+    //     rb[16-idx-1] = item
+    //     log.Printf("item = %v\n", item)
+    // }
+
+    // log.Printf("rb = %v\n", rb)
+
+
+    // i := binary.LittleEndian.Uint64(rb)
+
+    // log.Printf("i = %v\n", i)
+
+    log.Printf("key = %v\n", key)
+    // ---------github.com/reusee/mmh3------------
+    // ret := mmh3.Sum128(buffer.Bytes())
+    // log.Printf("ret = %v\n", ret)
+    // i, j := strconv.Atoi(string(ret))
+    // log.Printf("i = %v, %v\n", i, j)
+    // key = fmt.Sprintf("%v", ret)
 
     return key
 }
